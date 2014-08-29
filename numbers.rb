493ccec623fbb0cf6b7833baa2b54f96755aea46
MirokuNakazawa <nacl201402@gmail.com>
2014-08-29 10:12:57 +0900

    add

---
+++ numbers.rb
@@ -0,0 +1,22 @@
+#integer
+puts "integer"
+puts "p1+1:"
+p 1 + 1 #=>2
+puts "p 1-1:"
+p 1 - 1 #=>0
+puts "p 2 * 2:"
+p 2 * 2 #=>4
+puts "p 1 / 3:"
+p 1 / 3 #=>???
+
+#number
+puts"==== number"
+puts "p 1.0+1.0:"
+p 1.0 + 1.0 #=>2
+puts "p 1.0-1.0:"
+p 1.0 - 1.0 #=>0
+puts "p 2.0 * 2.0:"
+p 2.0 * 2.0 #=>4
+puts "p 1.0 / 3:"
+p 1.0 / 3   #=>???
+  
