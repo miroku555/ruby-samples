f72d085dd43b8c27edf68016177a58ed98e61f03
MirokuNakazawa <nacl201402@gmail.com>
2014-08-29 14:02:06 +0900

    ad

--- file3.rb
+++ file3.rb
@@ -1,5 +1,12 @@
-begin
-	File.open(ARGV[0],"w") do |io|
-rescue Exception => ex
-	p ex
+filename = "output.txt"
+i=0
+loop do
+	break unless File.exists?(filename)
+	filename = "output#{i}.txt"
+	puts filename
+	i+=1
+end
+
+File.open(filename,"w") do |io|
+	io.puts filename
 end
